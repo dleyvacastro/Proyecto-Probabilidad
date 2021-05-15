4bc86dba5bb74d395bb77a33cc1d9d32adf66dd0
Nico Quintero <juan24092002@gmail.com>
2021-05-15 10:41:01 -0500

    sentencias en R

--- entrega2/sentenciasr.R
+++ entrega2/sentenciasr.R
@@ -46,6 +46,30 @@
   # PIB USA
   boxplot(databaseactualizada$Pu, horizontal = TRUE, xlab = "Valor en millones de dolares", main = "PIB de Estados Unidos por trimestre")
   
-  # Inflación USA 
+  # Inflación Colombia
   boxplot(databaseactualizada$Ic, horizontal = TRUE, xlab = "Porcentaje", main = "Inflación de Colombia por trimestre")
+  
+  # Scatterplot de inflacion de colombia con importaciones de colombia
+  plot(databaseactualizada$Imc, databaseactualizada$Ic, xlab = "Importaciones Colombia", ylab = "Inflacion Colombia")
+
+  # Scatterplot de inflacion de colombia con exportaciones de colombia
+  plot(databaseactualizada$Ec, databaseactualizada$Ic, xlab = "Exportaciones Colombia", ylab = "Inflacion Colombia")
+  
+  # Scatterplot de exportaciones de colombia con importaciones de estados unidos
+  plot(databaseactualizada$Imu[0:29], databaseactualizada$Ec[0:29], xlab = "Importaciones Estados Unidos en millones de dolares", ylab = "Exportaciones Colombia en millones de pesos", xlim = c(500, 2500), ylim = c(20000, 35000), main = "Entre 2005 y 2012") 
+  
+  # Scatterplot de exportaciones de colombia con importaciones de estados unidos
+  plot( databaseactualizada$Imu[29:63], databaseactualizada$Ec[29:63],xlab = "Importaciones Estados Unidos en millones de dolares", ylab = "Exportaciones Colombia en millones de pesos", xlim = c(500, 2500), ylim = c(20000, 35000), main = "Entre 2012 y 2020") 
+  
+  # Scatterplot de importaciones de colombia con exportaciones de estados unidos
+  plot(databaseactualizada$Eu[0:29], databaseactualizada$Imc[0:29], xlab = "Exportaciones Estados Unidos en millones de dolares", ylab = "Importaciones Colombia en millones de pesos", xlim = c(400,2000), ylim = c(15000, 55000), main = "Entre 2005 y 2012")
+  
+  # Scatterplot de importaciones de colombia con exportaciones de estados unidos
+  plot(databaseactualizada$Eu[29:63], databaseactualizada$Imc[29:63], xlab = "Exportaciones Estados Unidos en millones de dolares", ylab = "Importaciones Colombia en millones de pesos", xlim = c(400,2000), ylim = c(15000, 55000), main = "Entre 2012 y 2020") 
+  
+  # Scatterplot de PIB de colombia con PIB de estados unidos
+  plot(databaseactualizada$Pu[0:29], databaseactualizada$Pc[0:29], xlab = "PIB Estados Unidos en millones de dolares", ylab = "PIB Colombia en millones de pesos", main = "Entre 2005 y 2012") 
+  
+  # Scatterplot de PIB de colombia con PIB de estados unidos
+  plot(databaseactualizada$Pu[29:63], databaseactualizada$Pc[29:63], xlab = "PIB Estados Unidos en millones de dolares", ylab = "PIB Colombia en millones de pesos", main = "Entre 2012 y 2005") 
   
